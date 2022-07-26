acac9022824df2e63b08f250c5db4485d64227ee
tknott95 <tknott95@hotmail.com>
2022-07-26 04:43:32 -0600

    scripts hotfix

--- scripts/test_post.sh
+++ scripts/test_post.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-curl --header "Content-Type: application/json" \
-  --request POST \
+curl \
+  -H "Content-Type: application/json" \
+  -X POST \
   --data '{"payoutAddr":"addr_tfkkkhfgfkhfk7lkrlqlnwm3xzzk88jc65c50a06ns46p0wjxe6xqkvnrs4f79wjp6tz07wrl2k2nctyqqkhtak","scriptAddr":"addr_test1qkggkgkk7lh0de4uzz03wyelc24kl7x4c5kay2rv4r6lz4ugu5yjaeraawcdgzztg8s6l3x4x","txOutLoc":"txAltTesting01","utxoUsing":"hdddghgdfhgdfhhgfjhgf#0"}' \
-  http://localhost:1335/api/post/tx
+  http://localhost:1335/api/post/tx | jq
+  
