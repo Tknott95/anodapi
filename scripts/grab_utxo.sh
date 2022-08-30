5d9bff6542864fd65d7708a11dd28a82c80c5cca
tknott95 <tknott95@hotmail.com>
2022-08-29 23:47:41 -0600

    misc

--- scripts/grab_utxo.sh
+++ scripts/grab_utxo.sh
@@ -10,4 +10,4 @@ addr_using=$1
 # might do this in haskell and just run on the binary
 # or both
 
-cardano-cli query utxo --address $addr_using $MAGIC | grep " 0 " | sed -n 2p | egrep  -o '[a-z0-9]+' | head -1
+cardano-cli query utxo --address $addr_using $MAGIC | grep " 0 " | sed -n 1p | egrep  -o '[a-z0-9]+' | head -1
